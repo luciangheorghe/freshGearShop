ce1a8f929f4f3dd1ada79e83b09dd0be7d3233c7
Lucian Gheorghe <lucian.gheorghe1989@gmail.com>
2019-10-20 00:51:54 +0300

    Product single page part 1

--- app/Controllers/Http/ProductController.js
+++ app/Controllers/Http/ProductController.js
@@ -9,6 +9,9 @@ class ProductController {
         const products = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
         return view.render('products/all', {products});
     }
+    show({view}) { 
+        return view.render('products/single');
+    }
 }
 
 module.exports = ProductController
--- start/routes.js
+++ start/routes.js
@@ -20,6 +20,8 @@ Route.get('/', 'PageController.home');
 Route.get('/about', 'PageController.about');
 
 Route.get('/products', 'ProductController.index');
+
+Route.get('/:brand/:subcategory/:slug', 'ProductController.show');
 Route.get('/products/new-arrivals', 'ProductController.newArrivals');
 
 // User Authentication
